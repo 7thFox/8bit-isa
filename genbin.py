2d52f7a224d7892509f32cc4034795480e8fe871
7thFox <dev7thFox@gmail.com>
2020-09-25 17:12:52 -0500

    add rest of instruction set

--- genbin.py
+++ genbin.py
@@ -1,17 +1,95 @@
 import numpy as np
 from words import OpCodes, CtlWrd, Flags
 
+# Program Space:    0x00-0xFF
+# Mem Space:        0x100-0x1FF
+
 microcode = {
     OpCodes.NOP: [],
     OpCodes.HLT: [CtlWrd.HALT],
+    OpCodes.RST: [CtlWrd.RST],
     OpCodes.ADD: [
         CtlWrd.A_IN | CtlWrd.STC_OE | CtlWrd.DEC_SC,
         CtlWrd.B_IN | CtlWrd.STC_OE,
-        CtlWrd.SUM_OE | CtlWrd.STC_IN | CtlWrd.INC_PC,
-        {
-            Flags.ZERO: 5,
-            Flags.CARRY: 4
-        }
+        CtlWrd.SUM_OE | CtlWrd.STC_IN | CtlWrd.FLG | CtlWrd.INC_PC,
+    ],
+    OpCodes.SUB: [
+        CtlWrd.A_IN | CtlWrd.STC_OE | CtlWrd.DEC_SC,
+        CtlWrd.B_IN | CtlWrd.STC_OE,
+        CtlWrd.SUM_OE | CtlWrd.SUB | CtlWrd.STC_IN | CtlWrd.FLG | CtlWrd.INC_PC,
+    ],
+    OpCodes.LST: [
+        CtlWrd.ROM_OE | CtlWrd.A_IN | CtlWrd.INC_SC,
+        CtlWrd.A_S1,
+        CtlWrd.A_OE | CtlWrd.STC_IN | CtlWrd.INC_PC
+    ],
+    OpCodes.RST: [
+        CtlWrd.ROM_OE | CtlWrd.A_IN | CtlWrd.INC_SC,
+        CtlWrd.A_S0,
+        CtlWrd.A_OE | CtlWrd.STC_IN | CtlWrd.INC_PC
+    ],
+    OpCodes.PSH: [
+        CtlWrd.ROM2A_NC | CtlWrd.INC_SC,
+        0x00,  # Data Load
+        CtlWrd.A_OE | CtlWrd.STC_IN | CtlWrd.INC_PC
+    ],
+    OpCodes.DUP: [
+        CtlWrd.STC_OE | CtlWrd.A_IN | CtlWrd.INC_SC,
+        CtlWrd.STC_IN | CtlWrd.A_OE | CtlWrd.INC_PC
+    ],
+    OpCodes.CAL: [
+        CtlWrd.INC_SC,
+        CtlWrd.PC_OE | CtlWrd.STC_IN | CtlWrd.ROM2A_NC,
+        0x00,  # Data Load
+        CtlWrd.PC_IN | CtlWrd.A_OE
+    ],
+    OpCodes.RTN: [
+        CtlWrd.STC_OE | CtlWrd.PC_IN | CtlWrd.DEC_SC
+    ],
+    OpCodes.BR: [
+        CtlWrd.ROM2A_NC,
+        0x00,  # Data Load
+        CtlWrd.PC_IN | CtlWrd.A_OE
+    ],
+    OpCodes.BEQ: [
+        [
+            {
+                "flags": set([x | Flags.ZERO for x in range(0b000, 0b111)]),
+                "op": CtlWrd.ROM2A_NC
+            },
+            {
+                "flags": set([x & Flags.NOT_ZERO for x in range(0b000, 0b111)]),
+                "op": CtlWrd.INC_PC
+            },
+        ],
+        [{
+            "flags": set([x | Flags.ZERO for x in range(0b000, 0b111)]),
+            "op": 0x00, # Data Load
+        }],
+        [{
+            "flags": set([x | Flags.ZERO for x in range(0b000, 0b111)]),
+            "op": CtlWrd.PC_IN | CtlWrd.A_OE
+        }],
+    ],
+    OpCodes.BNE: [
+        [
+            {
+                "flags": set([x | Flags.NOT_ZERO for x in range(0b000, 0b111)]),
+                "op": CtlWrd.ROM2A_NC
+            },
+            {
+                "flags": set([x & Flags.ZERO for x in range(0b000, 0b111)]),
+                "op": CtlWrd.INC_PC
+            },
+        ],
+        [{
+            "flags": set([x & Flags.NOT_ZERO for x in range(0b000, 0b111)]),
+            "op": 0x00, # Data Load
+        }],
+        [{
+            "flags": set([x & Flags.NOT_ZERO for x in range(0b000, 0b111)]),
+            "op": CtlWrd.PC_IN | CtlWrd.A_OE
+        }],
     ],
 }
 
@@ -22,11 +100,19 @@ FLAGS = 3
 # 3 * 8 = 24 bits
 rom = [np.uint32(0x00)] * (2**ADDR_BITS)
 
-def expandOp(op):
+def opt_with_flag(opts, flag):
+    for opt in opts:
+        if flag in opt["flags"]:
+            return opt["op"]
+    return 0x00
+
+def expand_op(op):
     if isinstance(op, int):
         return [op] * (2**FLAGS)
-    elif isinstance(op, dict):
-        return [op.get(x, 0x00) for x in range(0, 2**FLAGS)]
+    elif isinstance(op, list):
+        return [
+            opt_with_flag(op, f)
+            for f in range(0, 2**FLAGS)]
     else:
         assert False, "Unsupported op"
 
@@ -36,7 +122,7 @@ for opcode in microcode:
     ops = microcode[opcode]
     ops = ops + [0x00] * (2 ** STEPS - len(ops))
     assert not any(op != 0x00 for op in rom[addr_start:addr_end +1])
-    ops = sum([expandOp(op) for op in ops], [])
+    ops = sum([expand_op(op) for op in ops], [])
     rom[addr_start:addr_end + 1] = ops
     
 with open("rom1.bin", "wb") as out:
--- words.py
+++ words.py
@@ -1,12 +1,34 @@
 
 class Flags:
-    ZERO = 1 << 3
-    CARRY = 1 << 2
+    NO_FLAG = 0b000
+    ZERO = 0b001
+    CARRY = 0b010
+    UNASSIGNED = 0b100
+    
+    NOT_ZERO = 0b110
+    NOT_CARRY = 0b101
 
-class OpCodes: # 00-40
-    NOP = 0x01
-    HLT = 0x02
-    ADD = 0x03
+class OpCodes: # 00-3F (OO-1F80)
+    NOP = 0x00 # 0x00   000000
+    HLT = 0x01 # 0x80   000001
+    RST = 0x02 # 0x100  000010
+    
+    ADD = 0x08 # 0x400  001000
+    SUB = 0x09 # 0x480  001001
+    LST = 0x0A # 0x500  001010
+    RST = 0x0B # 0x580  001011
+    
+    PSH = 0x10 # 0x800  010000
+    POP = 0x11 # 0x880  010001
+    DUP = 0x12 # 0x900  010010
+    
+    CAL = 0x18 # 0xc00  011000
+    RTN = 0x19 # 0xc80  011001
+    
+    BR  = 0x20 # 0x1000 100000
+    BNE = 0x21 # 0x1080 100001
+    BEQ = 0x22 # 0x1100 100010
+
 
 class CtlWrd:
     # Chip 1
@@ -16,8 +38,7 @@ class CtlWrd:
     RST = 1 << 3
     INC_PC = 1 << 4
     FLG = 1 << 5
-    DAT = 1 << 6
-    SET_PC = 1 << 7
+    ROM2A_NC = 1 << 6
     # Chip 2
     A_OE = 1 << 8 + 0
     B_S0 = 1 << 8 + 1
@@ -31,6 +52,8 @@ class CtlWrd:
     ROM_OE = 1 << 16 + 0
     STC_OE = 1 << 16 + 1
     STC_IN = 1 << 16 + 2
+    PC_OE = 1 << 16 + 3
+    PC_IN = 1 << 16 + 4
     # Helpers
     A_IN = A_S0 | A_S1
     B_IN = B_S0 | B_S1
